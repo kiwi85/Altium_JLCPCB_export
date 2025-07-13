1635cb3765dfaaef44606f269fe4caaf41c54b8d
Julian <julain.huesinggit config --global user.email julian.huesing85@gmail.comgit config --global user.email julain.huesing>
2025-07-13 21:17:36 +0200

    promt for enter path of project output folder

--- process_altium_outputs.bat
+++ process_altium_outputs.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-REM === Set project path here ===
-set "PROJECT_DIR=C:\Users\julia\OneDrive\Projekte\PCBs\multi_sensor_core\Project Outputs for multi_sensor_core"
+REM === Ask user for project path ===
+set /p PROJECT_DIR="Enter the project path: "
 
 REM === Call Python script ===
 python "%~dp0process_jlcpcb_outputs.py" "%PROJECT_DIR%"
